43485f05fa0df3e82576ec9ff23e5d65ec8e7bad
cooooooooluser <85533967+cooooooooluser@users.noreply.github.com>
2022-01-19 18:03:38 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,26 @@ recognition.onresult = function run(e){
    var content = e.results[0][0].transcript;
    console.log(content);
    document.getElementById("textbox").innerHTML = content;
+   
+if(content == "Take my selfie." || content == "take my selfie"){
+    console.log("taking selfie");
+    speak();
+
+}
+
+}
+function speak() {
+    var talk = window.speechSynthesis;
+    var talking = "Taking your selfie in 5   4   3   2   1 BAM SELFIE";
+    var utterthis = new SpeechSynthesisUtterance(talking);
+    talk.speak(utterthis);
+   console.log(utterthis);
+   Webcam.attach(camera);
 } 
+
+Webcam.set({
+    width: 360, height:250 , image_format: "png", png_quality: 90 
+    
+});
+camera= document.getElementById("camera");
 
